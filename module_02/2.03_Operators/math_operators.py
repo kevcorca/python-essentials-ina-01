cde1d801c9f4e1e7eecdd3a187a2fe34aba75020
kevcorca <kevcorca@gmail.com>
2025-09-27 23:07:11 -0600

    Add Math Operators module 2.03

---
+++ module_02/2.03_Operators/math_operators.py
@@ -0,0 +1,26 @@
+#Math Operators
+
+# Basic Operators
+# Available
+# +, -, *, /, //, %, **
+
+# Addition
+print("Addition: ", 2 + 3)
+# Substraction
+print("Substraction: ", 3 - 1)
+# Multiplication
+print("Multiplication: ", 2 * 5)
+# Division
+print("Division: ", 7 / 2)
+# Integer Division
+print("Integer Division (floor): ", 7 / 2)
+# Modulo
+print("Modulo: ", 7 % 2)
+# Exponentiation
+print("Exponentiation:", 2 ** 2)
+
+# Priority List
+# 1. **
+# 2. Unarios
+# 3. *, /, //, %
+# 4. +, -
